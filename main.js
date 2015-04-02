f542bcdffbac11ba0dd4f9f501b346ac91f1c2b0
AndrewBrough <abrough@magmic.com>
2015-04-02 13:55:35 -0400

    added animate down functionality and fixed layout.

---
+++ main.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+  
+  var header = "header";
+  var mainContent = ".mainContent";
+
+  $(header).hover(function () {
+    console.log("hihihihi");
+    $(mainContent).animate({ top: "280px" }, 200);
+  },
+  function () {
+    console.log("byebyebye");
+    $(mainContent).animate({ top: "50px" }, 200);
+  });
+});
